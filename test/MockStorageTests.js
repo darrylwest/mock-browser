23e673674446afa7c1dfdd49d806c0e1025978b8
darryl west <darryl.west@RainCitySoftware.com>
2014-10-10 17:58:33 -0700

    storage tests and implementation

--- test/MockStorageTests.js
+++ test/MockStorageTests.js
@@ -12,27 +12,20 @@ var should = require('chai').should(),
 describe('MockStorage', function() {
     'use strict';
 
-    var createOptions = function() {
-        var opts = {};
-
-        opts.log = MockLogger.createLogger('MockStorage');
-
-        return opts;
-    };
-
     describe('#instance', function() {
-        var storage = new MockStorage( createOptions() ),
+        var storage = new MockStorage(),
             methods = [
                 'getItem',
                 'setItem',
-                'length',
                 'clear',
-                'key'
+                'key',
+                '__protected'
             ];
 
         it('should create an instance of MockStorage', function() {
             should.exist( storage );
             storage.should.be.instanceof( MockStorage );
+            storage.length.should.equal( 0 );
         });
 
         it('should have all known methods by size and type', function() {
@@ -40,6 +33,59 @@ describe('MockStorage', function() {
             methods.forEach(function(method) {
                 storage[ method ].should.be.a( 'function' );
             });
+        });
+    });
+
+    describe('setItem', function() {
+        var storage = new MockStorage();
+
+        it('should set a data item', function() {
+            var key = 'mykey',
+                value = 'my value';
+
+            storage.setItem( key, value );
+
+            storage.__protected().data[ key ].should.equal( value );
+        });
+    });
+
+    describe('getItem', function() {
+        var storage = new MockStorage();
+
+        it('should get a data item', function() {
+            var key = 'mykey',
+                value = 'my value';
+
+            storage.setItem( key, value );
+
+            storage.getItem( key ).should.equal( value );
+        });
+    });
+
+    describe('length', function() {
+        var storage = new MockStorage();
+
+        it('should get a data item', function() {
+            var key = 'mykey',
+                value = 'my value';
+
+            storage.setItem( key, value );
+
+            storage.length.should.equal( 1 );
+        });
+    });
+
+    describe('clear', function() {
+        var storage = new MockStorage();
+
+        it('should clear al data', function() {
+            for (var i = 0; i < 10; i++) {
+                storage.setItem( "key:" + i, 'value: ' + new Date().toJSON() );
+            }
+
+            storage.length.should.equal( 10 );
+            storage.clear();
+            storage.length.should.equal( 0 );
         });
     });
 });
